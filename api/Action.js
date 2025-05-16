33f9924a3a4edcd43ea77632bb53eb25d262d0b2
Ganhe Social <contato@ganhesocial.com>
2025-05-16 19:36:55 -0300

    Update Action.js

--- api/Action.js
+++ api/Action.js
@@ -7,7 +7,6 @@ const actionSchema = new mongoose.Schema({
     nome: String,
     valor: Number,
     quantidade: { type: Number, required: true },           // Total de vezes que essa ação deve ser executada
-    quantidadeExecutada: { type: Number, default: 0 },      // Contador de execuções já feitas
     link: String,
     status: { type: String, enum: ["pendente", "reservada", "concluida"], default: "pendente" },
     dataCriacao: { type: Date, default: Date.now }
