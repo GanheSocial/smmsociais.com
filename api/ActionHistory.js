2a1154980468b49370e5d87becb95db05de096b3
Ganhe Social <contato@ganhesocial.com>
2025-05-17 20:17:16 -0300

    Update ActionHistory.js

--- api/ActionHistory.js
+++ api/ActionHistory.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 
 const actionHistorySchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  token: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+  token: { type: String, required: false },
   nome_usuario: { type: String, required: true },
-  id_pedido: { type: mongoose.Schema.Types.ObjectId, required: true }, // Referência ao Action._id
-  id_conta: { type: String, required: true },
+  id_pedido: { type: String, required: true }, // Alterado de ObjectId para String
+  id_conta: { type: String, required: false }, // Agora opcional
   url_dir: { type: String, required: true },
-  tipo_acao: { type: String, required: true }, // Ex: "Seguir"
+  tipo_acao: { type: String, required: true },
   quantidade_pontos: { type: Number, required: true },
   tipo: { type: String, default: "Seguir" },
   rede_social: { type: String, default: "TikTok" },
-  valor_confirmacao: { type: String, required: true },
-  acao_validada: { type: Boolean, default: null }, // true, false ou null (pendente)
+  valor_confirmacao: { type: Number, default: 0 }, // Alterado de String para Number
+  acao_validada: { type: Boolean, default: null },
   data: { type: Date, default: Date.now }
 });
 
