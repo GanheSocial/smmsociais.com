306436d142561f27834b5bc1211fac4735dbff8f
Ganhe Social <contato@ganhesocial.com>
2025-05-28 18:52:44 -0300

    Update Action.js

--- api/Action.js
+++ api/Action.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const actionSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  id_servico: { type: String },
+  id_servico: { type: mongoose.Schema.Types.ObjectId, ref: "Servico" }, // Aqui como ObjectId referenciando 'Servico'
   rede: String,
   tipo: String,
   nome: String,
