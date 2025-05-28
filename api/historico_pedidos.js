cff907335effdb7ef6c7ddfb44b54f9591f8750c
Ganhe Social <contato@ganhesocial.com>
2025-05-28 19:40:28 -0300

    Update historico_pedidos.js

--- api/historico_pedidos.js
+++ api/historico_pedidos.js
@@ -29,9 +29,17 @@ const handler = async (req, res) => {
             filtro.status = status;
         }
 
-        const acoes = await Action.find(filtro).sort({ dataCriacao: -1 });
+const acoes = await Action.find(filtro)
+    .sort({ dataCriacao: -1 })
+    .populate({ path: "id_servico", model: Servico, select: "nome" });
 
-        return res.json({ acoes });
+const acoesComDetalhes = acoes.map(acao => {
+    const obj = acao.toObject();
+    obj.servicoDetalhes = obj.id_servico;
+    return obj;
+});
+
+return res.json({ acoes: acoesComDetalhes });
 
     } catch (error) {
         console.error("Erro ao buscar histórico de ações:", error);
