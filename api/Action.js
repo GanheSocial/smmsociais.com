d5b1bad5de28622e2cb0df9caee20b685a949aa8
Ganhe Social <contato@ganhesocial.com>
2025-05-28 00:20:58 -0300

    Update Action.js

--- api/Action.js
+++ api/Action.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const actionSchema = new mongoose.Schema({
-    rede: String,
-    tipo: String,
-    nome: String,
-    quantidade: { type: Number, required: true },           // Total de vezes que essa ação deve ser executada
-    quantidadeExecutada: { type: Number, default: 0 },      // Contador de execuções já feitas
-    link: String,
-    status: { type: String, enum: ["pendente", "reservada", "concluida"], default: "pendente" },
-    dataCriacao: { type: Date, default: Date.now }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  rede: String,
+  tipo: String,
+  nome: String,
+  quantidade: { type: Number, required: true },
+  quantidadeExecutada: { type: Number, default: 0 },
+  link: String,
+  status: { type: String, enum: ["pendente", "reservada", "concluida"], default: "pendente" },
+  dataCriacao: { type: Date, default: Date.now }
 });
 
 export const Action = mongoose.models.Action || mongoose.model("Action", actionSchema);
