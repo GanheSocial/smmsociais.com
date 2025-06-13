d9c946c97e2e9294c7806d2b82d062d124f5b597
SMM <contato@ganhesocial.com>
2025-06-12 23:30:34 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -654,40 +654,6 @@ const response = await fetch("https://api.mercadopago.com/v1/payments", {
   }
 }
 
-// Rota: /api/mailer
-if (url.startsWith("/api/mailer")) {
-
-      const transporter = nodemailer.createTransport({
-        host: 'smtpout.secureserver.net',
-        port: 465,
-        secure: true, // Porta 465 exige SSL
-        auth: {
-          user: 'contato@smmsocias.com',
-          pass: 'reno4769!', // sua senha real
-        },
-      });
-    
-      const mailOptions = {
-        from: '"SMMSociais" <contato@smmsociais.com>',
-        to: email,
-        subject: 'Recuperação de Senha',
-        html: `
-          <p>Você solicitou a recuperação de senha.</p>
-          <p>Clique no link abaixo para redefinir sua senha:</p>
-          <p><a href="${link}">${link}</a></p>
-          <p>Se você não solicitou essa recuperação, ignore este email.</p>
-        `,
-      };
-    
-      try {
-        await transporter.sendMail(mailOptions);
-        console.log(`Link de recuperação enviado para ${email}`);
-      } catch (error) {
-        console.error('Erro ao enviar email:', error);
-        throw new Error('Erro ao enviar email de recuperação');
-      }
-    }
-
  // Rota: /api/recover-password
 if (url.startsWith("/api/recover-password")) { 
   if (req.method !== "POST")
