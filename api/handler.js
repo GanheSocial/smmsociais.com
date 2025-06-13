4f70a815cfe4c4cf5464ce033ddd7dbeba8c02f2
SMM <contato@ganhesocial.com>
2025-06-12 23:35:20 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -803,7 +803,29 @@ if (url.startsWith("/api/change-password")) {
         } catch (error) {
             return res.status(500).json({ error: "Erro ao validar token" });
         }
-    };   
+    };
+    
+  // Rota: api/getMessages
+ if (url.startsWith("/api/getMessages")) { 
+      if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Método não permitido, use GET.' });
+  }
+
+  await connectDB();
+
+  const { session_id } = req.query;
+
+  if (!session_id) {
+    return res.status(400).json({ error: 'session_id obrigatório.' });
+  }
+
+  try {
+    const messages = await Message.find({ session_id }).sort({ timestamp: 1 });
+    return res.status(200).json({ messages });
+  } catch (error) {
+    return res.status(500).json({ error: 'Erro ao buscar mensagens.' });
+  }
+}
 
     return res.status(404).json({ error: "Rota não encontrada." });
 }
